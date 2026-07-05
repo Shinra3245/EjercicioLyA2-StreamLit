9020f65bc90c8655483daac45def220e155435f1
AndyMonjaras <andresmonjaras98@gmail.com>
2026-07-04 20:05:55 -0600

    Ejercicio12: Completado y funcional

---
+++ ejercicio12/text.py
@@ -0,0 +1,29 @@
+# Importa ANTLR4 para funciones
+from antlr4 import *
+from ExprLexer import ExprLexer
+import sys
+
+# Comprobar si se paso mas de un argumento
+if len(sys.argv) > 1:
+    # entrada por archivo
+    input_stream = FileStream(sys.argv[1],  encoding='utf-8')
+else:
+    # entrada por terminal
+    input_stream = InputStream(input("? "))
+
+
+lexer = ExprLexer(input_stream)
+# Toma los tokens que produjo el lexer y los guarda en un flujo/lista
+tokens = CommonTokenStream(lexer)
+tokens.fill()
+print(tokens)
+
+
+for token in tokens.tokens:
+    print("Texto", token.text)
+    print("Linea", token.line)
+    print("Columna", token.column)
+    nombre_token = lexer.symbolicNames[token.type]
+    print("Tipo", nombre_token)
+
+    print("------------------------")
